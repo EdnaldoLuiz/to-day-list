90ed21bd0f1fecfc0aa58bae4bc319c728cc4ffb
Ednaldo Luiz <ednaldo.eln68@gmail.com>
2023-10-18 19:37:45 -0300

    refactoring cors

--- src/main/java/br/com/luiz/todolist/infra/config/CorsConfig.java
+++ src/main/java/br/com/luiz/todolist/infra/config/CorsConfig.java
@@ -22,7 +22,7 @@ public class CorsConfig implements Filter {
             throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
         response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         response.setHeader("Access-Control-Max-Age", "3600");
         chain.doFilter(req, res);
