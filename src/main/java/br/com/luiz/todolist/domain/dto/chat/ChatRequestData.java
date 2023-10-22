8b34a926cf858e0ff686df78a81ab43a39579b40
Ednaldo Luiz <ednaldo.eln68@gmail.com>
2023-10-22 17:58:27 -0300

    renaming, adding data to match other nomenclatures

--- src/main/java/br/com/luiz/todolist/domain/dto/chat/ChatRequestData.java
+++ src/main/java/br/com/luiz/todolist/domain/dto/chat/ChatRequestData.java
@@ -9,12 +9,12 @@ import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-public class ChatRequest {
+public class ChatRequestData {
 
     private String model;
     private List<MessageModel> messages;
 
-    public ChatRequest(String model, String prompt) {
+    public ChatRequestData(String model, String prompt) {
         this.model = model;
         messages = new ArrayList<>();
         messages.add(new MessageModel("user", prompt));
--- src/main/java/br/com/luiz/todolist/domain/dto/chat/ChatResponseData.java
+++ src/main/java/br/com/luiz/todolist/domain/dto/chat/ChatResponseData.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class ChatResponse {
+public class ChatResponseData {
     
     private List<Choice> choices;
 
