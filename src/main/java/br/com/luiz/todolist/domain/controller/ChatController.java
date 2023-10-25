cf56e382be0de2419bf2f66746f0959a3e8dfa3b
Ednaldo Luiz <ednaldo.eln68@gmail.com>
2023-10-24 21:35:13 -0300

    refactoring the code, add the two new exceptions again

--- src/main/java/br/com/luiz/todolist/domain/controller/ChatController.java
+++ src/main/java/br/com/luiz/todolist/domain/controller/ChatController.java
@@ -14,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 import br.com.luiz.todolist.domain.dto.chat.ChatRequestData;
 import br.com.luiz.todolist.domain.dto.chat.ChatResponseData;
 import br.com.luiz.todolist.domain.model.MessageModel;
+import br.com.luiz.todolist.infra.exception.InvalidChatMessageException;
 
 @RestController
 @RequestMapping
--- src/main/java/br/com/luiz/todolist/infra/exception/InvalidChatMessageException.java
+++ src/main/java/br/com/luiz/todolist/infra/exception/InvalidChatMessageException.java
@@ -1,11 +1,11 @@
-package br.com.luiz.todolist.domain.controller;
+package br.com.luiz.todolist.infra.exception;
 
 import br.com.luiz.todolist.domain.model.MessageModel;
 
 public class InvalidChatMessageException extends RuntimeException {
 
     public InvalidChatMessageException(MessageModel messageModel) {
-        
+
     }
 
 }
