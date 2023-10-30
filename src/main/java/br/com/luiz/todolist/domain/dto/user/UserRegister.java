fa9b0f82a3a7d33bb154cc52986627809f32996e
Ednaldo Luiz <ednaldo.eln68@gmail.com>
2023-10-29 23:54:11 -0300

    enhancing UserRegister validation

--- src/main/java/br/com/luiz/todolist/domain/dto/user/UserRegister.java
+++ src/main/java/br/com/luiz/todolist/domain/dto/user/UserRegister.java
@@ -1,14 +1,19 @@
 package br.com.luiz.todolist.domain.dto.user;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record UserRegister(
 
     @NotBlank
+    @Email
     String login,
 
     @NotBlank
+    @Size(min = 8)
     String password,
     
     @NotBlank
+    @Size(min = 3)
     String username) {}
